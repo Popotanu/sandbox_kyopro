c1cbf1afcb6f1e40be83a34c8000392245a6d631
popotanu <scrapmacaron@gmail.com>
2022-08-29 20:26:24 +0900

    [WA]abc095_c_half_and_half:WAしたらcommitするのを試す.
    どう間違えたかをのこしておきたい

---
+++ abc095/C/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+  int A, B, C;
+  int X, Y;
+
+  cin >> A >> B >> C >> X >> Y;
+
+  // 最初に全部ABピザで賄おうとする
+  // 1枚ずつABピザを減らして，その分をAピザBピザで補う
+  // ABピザの初期枚数:max(X,Y)*2枚
+  // から0枚までABピザを1枚ずつ減らし，価格の総和のminをとる
+  int ans = C * max(X, Y) * 2;
+  rrep(i, max(X, Y), 0) {
+    if (i % 2) continue;
+    int c = C * i * 2;
+    int a = A * max(0, X - i);
+    int b = B * max(0, Y - i);
+    ans = min(ans, a + b + c);
+  }
+
+  cout << ans << endl;
+}
