95f2a2de700cbc6a8d108a9d05ec401944adc12b
popotanu <scrapmacaron@gmail.com>
2022-08-31 02:49:49 +0900

    [AC]abc085_b_kagami_mochi

---
+++ abc085/B/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+
+int N;
+string A[1009];
+int main() {
+  cin >> N;
+  set<int> d;
+  rep(i, 0, N) {
+    int dd;
+    cin >> dd;
+    d.insert(dd);
+  }
+
+  cout << d.size() << endl;
+}
