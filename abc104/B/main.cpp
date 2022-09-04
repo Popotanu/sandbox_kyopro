203f6c0f256592254a12c92ee75e20c9a0b6a344
popotanu <scrapmacaron@gmail.com>
2022-09-04 15:40:24 +0900

    [AC]abc104_b_AcCepted: さくっとregex書く.つい脊髄反射的にループ回したくなるけど

---
+++ abc104/B/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+
+int N;
+string A;
+int main() {
+  cin >> A;
+  regex re("^A[a-z]+C[a-z]+$");
+  cout << (regex_search(A, re) ? "AC" : "WA") << endl;
+}
