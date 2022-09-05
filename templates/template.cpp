f6fe53732f798a6139df561c00feff4efb584da7
popotanu <scrapmacaron@gmail.com>
2022-09-06 00:57:55 +0900

    add some cool macros/templates
    ref: https://blog.hamayanhamayan.com/entry/2021/05/13/211037

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// clang-format off
 #define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
 #ifdef LOCAL
 #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
@@ -14,6 +15,11 @@ using namespace std;
 #define rrep(i, a, b) for (int i = a; i >= b; i--)
 #define fore(i, a) for (auto &i : a)
 #define all(x) (x).begin(), (x).end()
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
 
 int N;
 string A;
