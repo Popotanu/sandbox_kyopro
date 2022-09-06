3c91ae37c2b5609d4cbd5a801ca16d3678ca7afa
popotanu <scrapmacaron@gmail.com>
2022-09-07 08:29:21 +0900

    [WA]abc166_c_peaks

---
+++ abc166/C/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// clang-format off
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N, M;
+string A;
+void _main() {
+  cin >> N >> M;
+  vector<int> H(N);
+  fore(i, H) cin >> i;
+
+  vector<vector<int>> G(N);
+  rep(i, 0, M) {
+    int u, v;
+    cin >> u >> v;
+    u--, v--;
+    G[u].push_back(v);
+    G[v].push_back(u);
+  }
+
+  int ans = 0;
+  rep(i, 0, G.size()) {
+    bool highest = true;
+    rep(j, 0, G[i].size()) {
+      if (H[i] <= H[G[i][j]]) highest = false;
+      break;
+    }
+    if (highest) ans++;
+  }
+
+  cout << ans << endl;
+}
