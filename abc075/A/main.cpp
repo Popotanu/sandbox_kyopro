82f25488bbc45aeb0b635adf28676fbe7822ba2e
popotanu <scrapmacaron@gmail.com>
2022-09-09 23:52:04 +0900

    [AC]abc075_a: XOR取ればいいじゃん．ってすぐに思いつくようになりたい

--- abc075/A/main.cpp
+++ abc075/A/main.cpp
@@ -21,10 +21,10 @@ template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } 
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 // clang-format on
 
-int N;
+int N, M, L;
 string A;
 void _main() {
+  cin >> N >> M >> L;
 
-  cin >> N;
-  // cout << N << endl;
+  cout << (N ^ M ^ L) << endl;
 }
