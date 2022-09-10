283e78e15e738c55831b069730d1ceea3d456cc8
popotanu <scrapmacaron@gmail.com>
2022-09-10 14:01:16 +0900

    [解説AC]abc075_c_bridge: 別解:LowLinkを使って橋を数える
    - 時間計算量がO(V+E)で早い
    - DFSで辿らなかった辺(後退辺)をうまく利用する

--- abc075/C/main.cpp
+++ abc075/C/main.cpp
@@ -21,59 +21,77 @@ template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } 
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 // clang-format on
 
-struct UnionFind {
-  vector<int> par, siz;
+struct LowLink {
+  const vector<vector<int>>& G;
 
-  UnionFind(int n) : par(n, -1), siz(n, 1) {}
+  // ord[u] < low[v]ならば(u,v)は橋.
+  // (じつは<=も成り立つ)
+  vector<int> seen, ord, low;
+  vector<int> aps;  // 関節点. articulation points.
+  vector<pair<long long, long long>> bridges;
 
-  int root(int x) {
-    if (par[x] == -1) return x;
-    return root(par[x]);
+  LowLink(const vector<vector<int>>& G_) : G(G_) {
+    seen.assign(G.size(), 0);
+    ord.assign(G.size(), 0);
+    low.assign(G.size(), 0);
+    int k = 0;
+
+    for (int i = 0; i < G.size(); i++) {
+      if (!seen[i]) k = dfs(i, k, -1);
+    }
+
+    // 必要ならsortする
+    // sort(aps.begin(), aps.end());
+    // sort(bridges.begin(), bridges.end());
   }
 
-  bool is_same(int x, int y) { return root(x) == root(y); }
+  int dfs(const int now, int& k, const int par) {
+    seen[now] = true;
+    ord[now] = k++;  // 探索順の記録
+    low[now] = ord[now];
 
-  bool unite(int x, int y) {
-    x = root(x);
-    y = root(y);
-    if (x == y) return false;
-    if (x < y) swap(x, y);
+    bool is_aps = false;
+    int children = 0;
 
-    par[y] = x;
-    siz[x] += siz[y];
-    return true;
+    for (auto& v : G[now]) {
+      // vが未探索なら,辺{now,v}を使ってvへ行く
+      if (!seen[v]) {
+        children++;
+
+        k = dfs(v, k, now);
+
+        // 辺{自分,子}がDFSで辿ったものならば,
+        // 自分のLowLinkは子のLowLink以下なはず
+        low[now] = min(low[now], low[v]);
+        if (par != -1 && ord[now] <= low[v]) is_aps = true;
+        if (ord[now] < low[v]) bridges.emplace_back(min(now, v), max(now, v));
+
+      } else if (v != par) {
+        // vが訪問済だけど,DFSで辺{now,v}を通らなかった({now,v}が後退辺)
+        // 1回だけ後退辺を辿れる
+        low[now] = min(low[now], ord[v]);
+      }
+    }
+
+    // DFS treeの根で,子が2つ以上あるなら関節点
+    if (par == -1 && children >= 2) is_aps = true;
+    if (is_aps) aps.push_back(now);
+    return k;
   }
-
-  int size(int x) { return siz[root(x)]; }
 };
 
 int N, M;
 void _main() {
-  // 単純連結無向グラフの橋の本数を求める
-  // N <=50, M <=50
   cin >> N >> M;
-
-  vector<int> A(M), B(M);
-
+  vector<vector<int>> G(N);
   rep(i, 0, M) {
-    cin >> A[i] >> B[i];
-    A[i]--;
-    B[i]--;
+    int u, v;
+    cin >> u >> v;
+    u--, v--;
+    G[u].push_back(v);
+    G[v].push_back(u);
   }
 
-  int ans = 0;
-  rep(i, 0, M) {
-    UnionFind uf(N);
-    rep(j, 0, M) {
-      if (i != j) uf.unite(A[j], B[j]);
-    }
-    rep(k, 0, N - 1) {
-      if (!uf.is_same(N - 1, k)) {
-        ans++;
-        break;
-      }
-    }
-  }
-
-  cout << ans << endl;
+  LowLink lowlink(G);
+  cout << lowlink.bridges.size() << endl;
 }
