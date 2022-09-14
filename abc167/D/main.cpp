bccf7e627fd563796fc0cd37ea682285beaeb4bf
popotanu <scrapmacaron@gmail.com>
2022-09-14 17:39:26 +0900

    [AC]abc167_d_telepoter: indexの処理がおそいおそい...
    - 境界の処理でいつもあわわわしちゃう...
    - 1発ACしてえらい

--- abc167/D/main.cpp
+++ abc167/D/main.cpp
@@ -22,9 +22,85 @@ template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } 
 // clang-format on
 
 int N;
+ll obsession;
 string A;
 void _main() {
+  cin >> N >> obsession;
 
-  cin >> N;
-  // cout << N << endl;
+  // x_i+1 = σ(x_i) = A[x_i]
+  // 最初のxをx=0として，obsession回σでうつすと どの要素にいる？？
+
+  // e.g.1)
+  // 4 5
+  // 3 2 4 1
+  // =>4
+
+  // (全単射じゃないから置換じゃないけど)
+  // 巡回置換の積のようなものを考えて,各項ごとに分ければいいかな？
+  // 始点が0で固定されてるから, 0はじまりの巡回だけ考えれば良さそう
+
+  // テレポでたどる町をlistに記録してく.walkとする.
+  // i回目のテレポでwalk[i]にいる.
+  // ただし，0回目としてwalk[0] = 0 にしておく.
+
+  // 1.walk.size() == Nになったとき:
+  //   walkは全部の町の集合を包む.
+  //   全部の町に1回ずつ行ったあと, 0に戻る.ループする.
+  //   walk[obsession % N]を取ればいい.
+
+  // 2. j回目のテレポで，walk[j] == j になった:
+  //   j回目以降のテレポはずっと今いる街に飛び続ける.かわいそう.
+  //    - walk[obsession] (obsession<j)
+  //    - walk[j] (j <= obsession)
+
+  // 3. 2は満たさないけど, j回目のテレポで以前行った町Aに戻った:
+  //    (かわいそうって思ったけど現実とそう変わらない事に気づいた.)
+  //
+  //    Aを最初に訪れたのがk回目とする．つまりwalk[k] == A
+  //    [walk[k], walk[j-1]]でループし続ける.
+  //    - walk[obsession] (obsession<j)
+  //    - walk[k + (obsession-k) % (ループに含まれる街の数)]
+
+  // 巡回を調べるのにO(N)
+  // N<=2*10^5だからいけそう
+  // 行った町の検索がしんどい. walkとは別に setで行った町の集合を持つことにする.
+
+  vector<int> A(N);
+  fore(i, A) {
+    int b;
+    cin >> b;
+    i = b - 1;
+  }
+
+  vector<int> walk;
+  set<int> visited;
+  walk.push_back(0);
+  visited.insert(0);
+
+  int be = 0;
+  int now = 0;
+  rep(i, 1, N) {
+    int next = A[now];
+
+    if (next == now || visited.count(next)) {
+      be = distance(walk.begin(), find(all(walk), next));
+      break;
+    }
+
+    walk.push_back(next);
+    visited.insert(next);
+    now = next;
+  }
+
+  int ans = 0;
+
+  if (walk.size() == N) {
+    ans = walk[obsession % N];
+  } else if (obsession < be) {
+    ans = walk[obsession];
+  } else {
+    ans = walk[be + (obsession - be) % (walk.size() - be)];
+  }
+
+  cout << ++ans << endl;
 }
