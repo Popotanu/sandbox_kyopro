55368bda15e934642dfb4ca12037ce00c45710f5
popotanu <scrapmacaron@gmail.com>
2022-09-19 02:17:38 +0900

    [AC]abc153_d: 再帰

---
+++ abc153/D/main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+ll H;
+string S;
+
+ll beat(stack<ll>& mo) {
+  if (mo.empty()) return 0;
+
+  ll hp = mo.top();
+  mo.pop();
+
+  if (hp == 1) {
+    return 1 + beat(mo);
+  } else {
+    mo.push(hp / 2);
+    return 1 + beat(mo) * 2;
+  }
+}
+
+void _main() {
+  cin >> H;
+
+  stack<ll> mo;
+  mo.push(H);
+
+  std::cout << beat(mo) << endl;
+}
