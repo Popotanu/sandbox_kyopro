8b1be58ade2038b32b450b1dbf9b786ef3e4452d
popotanu <scrapmacaron@gmail.com>
2022-09-19 01:09:49 +0900

    [解説AC]abc165_c_many_requirements:全探索+再帰．鹿本

--- abc165/C/main.cpp
+++ abc165/C/main.cpp
@@ -21,10 +21,58 @@ template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } 
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 // clang-format on
 
-int N;
+int N, M, Q;
 string A;
-void _main() {
+vector<ll> a, b, c, d;
 
-  cin >> N;
-  // cout << N << endl;
+ll calc_score(vector<int>& A) {
+  ll score = 0;
+  rep(i, 0, Q) {
+    if (A[b[i]] - A[a[i]] == c[i]) {
+      score += d[i];
+    }
+  }
+  return score;
+}
+
+ll rec(vector<int>& A) {
+  if (A.size() == N) return calc_score(A);
+
+  ll result = 0;
+  int prev = (A.empty() ? 1 : A.back());
+
+  // ここで数列Aを構成していく
+  // 試しては捨て，試しては捨て....
+  rep(i, prev, M + 1) {
+    A.push_back(i);
+    chmax(result, rec(A));
+    A.pop_back();
+  }
+
+  return result;
+}
+void _main() {
+  cin >> N >> M >> Q;
+  a.resize(Q);
+  b.resize(Q);
+  c.resize(Q);
+  d.resize(Q);
+  rep(q, 0, Q) {
+    cin >> a[q] >> b[q] >> c[q] >> d[q];
+    a[q]--, b[q]--;
+  }
+
+  /*
+  1<= A1<= A2 <= A_3 <= M=4
+  得点が最大になるようにAを定める.
+
+  3 4 3
+  a b c d
+  1 3 3 100 A3-A1 = 3 A={1,x,4}
+  1 2 2 10            A={1,3,x}
+  2 3 2 10            A={x,2,4}とか
+*/
+
+  vector<int> A;
+  cout << rec(A) << endl;
 }
