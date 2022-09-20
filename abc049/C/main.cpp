ed1b346d7d4bb6a736e50b7fd0735ae10bd070fe
popotanu <scrapmacaron@gmail.com>
2022-09-21 04:14:32 +0900

    [解説AC]abc049_c_白昼夢: DPチュートリアル.

---
+++ abc049/C/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N;
+string S, T;
+void _main() {
+  cin >> S;  // <=10e5
+  N = S.size();
+
+  vector<string> candidates = {"dream", "dreamer", "erase", "eraser"};
+  vector<bool> dp(S.size() + 1, false);
+  dp[0] = true;  // 0文字目はtrue
+
+  rep(i, 1, N + 1) {
+    fore(c, candidates) {
+      int l = c.size();
+      if (i >= l && dp[i - l] && S.substr(i - l, l) == c) dp[i] = true;
+    }
+  }
+
+  assert(dp[0] == true);
+  string ans = dp[N] ? "YES" : "NO";
+  cout << ans << endl;
+}
