6007d8b6b4f2f851b61e5d6efa3435e8ace96489
popotanu <scrapmacaron@gmail.com>
2022-09-23 02:21:44 +0900

    [AC]abc140_c_maximal_value

---
+++ abc140/C/main.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+// https://atcoder.jp/contests/abc140/tasks/abc140_c
+int N;
+string S;
+void _main() {
+  cin >> N;
+  vector<int> B(N - 1);
+  fore(i, B) cin >> i;
+
+  int ans = B[0];
+  rep(i, 1, B.size()) {
+    int a = min(B[i - 1], B[i]);
+    ans += a;
+  }
+  ans += B.back();
+
+  cout << ans << endl;
+}
