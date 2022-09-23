673b4b84daa645b297150cf1ea924d41a912192a
popotanu <scrapmacaron@gmail.com>
2022-09-23 21:43:56 +0900

    [解説AC]abc129_c: DPチュートリアル.ミスがミスをよんでひどかった
    - そもそも解法の間違いに気づかなかった
    - 自分の，間違った実装に合わせるために前提を捻じ曲げた
    - 10e9+7のmodとった(1e9+7じゃなくて)
    - 再帰で書いたらTLEした

---
+++ abc129/C/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#ifdef LOCAL
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+// #define dbg(x) true
+#endif
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N, M;
+string S;
+vector<ll> dp;
+vector<bool> A;
+
+void _main() {
+  cin >> N >> M;
+  A.assign(N + 1, true);
+  rep(i, 0, M) {
+    int a;
+    cin >> a;
+    A[a] = false;
+  }
+
+  dp.assign(N + 1, 0);
+  dp[0] = 1;
+  rep(i, 1, N + 1) {
+    if (i - 1 >= 0 && A[i - 1]) dp[i] += dp[i - 1];
+    if (i - 2 >= 0 && A[i - 2]) dp[i] += dp[i - 2];
+    dp[i] %= ((int)1e9 + 7);
+  }
+  cout << dp[N] << endl;
+}
