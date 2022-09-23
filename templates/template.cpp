7966cb2f7ab80951ab31f048c7a323b49f4537e6
popotanu <scrapmacaron@gmail.com>
2022-09-24 03:38:47 +0900

    dont let call functions for debugging on online judge

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 /* -------------ACL--------------- */
 // #include <atcoder/fenwicktree>
 // #include <atcoder/segtree>
@@ -22,30 +21,29 @@ using namespace std;
 // using namespace atcoder;
 /* ------------------------------- */
 
-
 // clang-format off
-#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
 #ifdef LOCAL
-#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+  #define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
 #else
-#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
-// #define dbg(x) true
+  #define dbg(x) true
 #endif
 
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 #define rep(i, a, b) for (int i = a; i < b; i++)
 #define rrep(i, a, b) for (int i = a; i >= b; i--)
 #define fore(i, a) for (auto &i : a)
 #define all(x) (x).begin(), (x).end()
-void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 // clang-format on
 
+#define LOCAL
+
 int N;
 string S;
 void _main() {
-
   cin >> N;
 
   int ans = 0;
