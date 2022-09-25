4f0c265ea09f55afc9de534bfbd5a1618ce18ae1
popotanu <scrapmacaron@gmail.com>
2022-09-26 01:29:59 +0900

    [AC]abc138_c_alchemist:貪欲法チュートリアル.公式解説がおもしろい

---
+++ abc138/C/main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define LOCAL
+
+#ifdef LOCAL
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+  #define dbg(x) true
+#endif
+
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N, M;
+string S;
+void _main() {
+  cin >> N;
+
+  priority_queue<double, std::vector<double>, std::greater<double>> v;
+
+  rep(i, 0, N) {
+    double a;
+    cin >> a;
+    v.push(a);
+  }
+
+  double ans;
+  while (v.size() > 1) {
+    ans = v.top();
+    v.pop();
+    ans += v.top();
+    ans /= 2.0;
+    v.pop();
+
+    v.push(ans);
+  }
+
+  cout << fixed << setprecision(10);
+  cout << ans << endl;
+}
