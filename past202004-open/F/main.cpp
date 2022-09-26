54672ba939a1ad76b5d0e235760fa6c1f8e73d05
popotanu <scrapmacaron@gmail.com>
2022-09-26 21:22:03 +0900

    [AC]past02_f_tasking: 貪欲法.問題文読み違えて最終日からheapにつめてってた

--- past202004-open/F/main.cpp
+++ past202004-open/F/main.cpp
@@ -2,29 +2,66 @@
 
 using namespace std;
 
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
 // clang-format off
-#define _GLIBCXX_DEBUG  // 空の構造に対して未定義な操作を行ったときに例外を投げる
+#define LOCAL
+
 #ifdef LOCAL
-#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
 #else
-#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
-// #define dbg(x) true
+  #define dbg(x) true
 #endif
 
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 #define rep(i, a, b) for (int i = a; i < b; i++)
 #define rrep(i, a, b) for (int i = a; i >= b; i--)
 #define fore(i, a) for (auto &i : a)
 #define all(x) (x).begin(), (x).end()
-void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 // clang-format on
 
-int N;
-string A;
+int N, M;
+string S;
 void _main() {
-
   cin >> N;
-  // cout << N << endl;
+  map<int, vector<int>> A;
+  rep(i, 0, N) {
+    int a, b;
+    cin >> a >> b;
+    A[--a].push_back(b);
+  }
+
+  vector<int> ans;
+  priority_queue<int> que;
+  rep(i, 0, N) {
+    fore(wage, A[i]) { que.push(wage); }
+    ans.push_back(que.top());
+    que.pop();
+  }
+
+  accumulate(ans.begin(), ans.end(), 0, [](int acc, const auto& current) {
+    std::cout << acc + current << endl;
+    return acc + current;
+  });
 }
