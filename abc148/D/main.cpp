f3ce4fc1e967614fa8a4db1fe16ae2d1d11364f7
popotanu <scrapmacaron@gmail.com>
2022-09-26 17:36:26 +0900

    [AC]abc148_d: 貪欲に取っていく
    - その要素を選んでも最終的な結果が悪くなることがないよね？を丁寧に確認していく

---
+++ abc148/D/main.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define LOCAL
+
+#ifdef LOCAL
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+  #define dbg(x) true
+#endif
+
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N;
+string S;
+void _main() {
+  cin >> N;
+  vector<int> A(N);
+  bool available = false;
+  fore(i, A) {
+    int a;
+    cin >> a;
+    if (a == 1) available = true;
+    i = a;
+  }
+
+  if (!available) {
+    cout << -1 << endl;
+    return;
+  }
+
+  int ans = 0, cur = 0;
+  fore(i, A) {
+    if (i == cur + 1) {
+      cur++;
+    } else {
+      ans++;
+    }
+  }
+
+  cout << ans << endl;
+}
