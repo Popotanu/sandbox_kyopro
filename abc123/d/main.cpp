1bd1ad9547d7add9e65a55c11d348146d6dbda34
popotanu <scrapmacaron@gmail.com>
2022-09-28 14:47:01 +0900

    tm

---
+++ abc123/d/main.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define LOCAL
+
+#ifdef LOCAL
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+  #define dbg(x) true
+#endif
+
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int X, Y, Z, K;
+void _main() {
+  cin >> X >> Y >> Z >> K;
+  vector<ll> A(X), B(Y), C(Z);
+  fore(i, A) cin >> i;
+  fore(i, B) cin >> i;
+  fore(i, C) cin >> i;
+  dbg(A[K - 1]);
+  dbg("=======");
+
+  priority_queue<ll> sum;
+  fore(a, A) {
+    fore(b, B) {
+      fore(c, C) { sum.push(a + b + c); }
+    }
+  }
+
+  dbg(sum.size());
+
+  dbg(X * Y * Z);
+  while (!sum.empty()) {
+    cout << sum.top() << endl;
+    sum.pop();
+  }
+}
