1e8d5fa8250ebbfd80389f334f2f1eaf8b28b4f7
popotanu <scrapmacaron@gmail.com>
2022-09-29 21:29:51 +0900

    [AC]abc103_d_islands_war:区間スケジューリング問題.後ろからみてく

--- abc103/D/main.cpp
+++ abc103/D/main.cpp
@@ -2,23 +2,73 @@
 
 using namespace std;
 
-#define _GLIBCXX_DEBUG // 空の構造に対して未定義な操作を行ったときに例外を投げる
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+
 #ifdef LOCAL
-#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
 #else
-#define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
-// #define dbg(x) true
+  #define dbg(x) true
 #endif
 
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 #define rep(i, a, b) for (int i = a; i < b; i++)
 #define rrep(i, a, b) for (int i = a; i >= b; i--)
 #define fore(i, a) for (auto &i : a)
 #define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
 
-int N;
-string A[1009];
-int main() {
+int N, M;
+string S;
+void _main() {
+  cin >> N >> M;
 
-  cin >> N;
-  // cout << N << endl;
+  // {終点,始点}
+  vector<pair<int, int>> P(M);
+  fore(i, P) {
+    cin >> i.second >> i.first;
+    --i.first, --i.second;
+  }
+
+  sort(all(P));
+
+  int ans = 0;
+  set<int> seen;
+  rep(i, 0, M) {
+    if (seen.count(i)) continue;
+    seen.insert(i);
+    ans++;
+    int st = P[i].second, en = P[i].first;
+    rep(j, i + 1, M) {
+      if (seen.count(j)) continue;
+      dbg(P[j].second);
+      if (P[j].second < en) {
+        seen.insert(j);
+      }
+    }
+  }
+
+  std::cout << ans << endl;
 }
