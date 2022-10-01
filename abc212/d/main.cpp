adfe3f2d0dbd8d047e8b4021c5bdd2a88b17e93c
popotanu <scrapmacaron@gmail.com>
2022-10-01 16:21:38 +0900

    [解説AC]abc212_d_querying_multiset: 累積和をうまくつかう

---
+++ abc212/d/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+#include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define LOCAL
+
+#ifdef LOCAL
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+  #define dbg(x) true
+#endif
+
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N, M, Q;
+void _main() {
+  cin >> Q;
+
+  priority_queue<ll, vector<ll>, greater<ll>> qu;
+  ll sum = 0;
+  rep(i, 0, Q) {
+    int t;
+    ll x;
+    cin >> t;
+    if (t == 1) {
+      cin >> x;
+      qu.push(x - sum);
+    } else if (t == 2) {
+      cin >> x;
+      sum += x;
+
+    } else {
+      cout << 1LL * qu.top() + sum << endl;
+      qu.pop();
+    }
+  }
+}
