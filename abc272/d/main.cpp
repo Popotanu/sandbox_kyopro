c436ff5927c82df20a0dc505fe9b7b87fbff8456
popotanu <scrapmacaron@gmail.com>
2022-10-11 13:44:54 +0900

    [AC]abc272_d

---
+++ abc272/d/main.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+#define LOCAL
+
+#ifdef LOCAL
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+  #define dbg(x) true
+#endif
+
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N, M;
+string S;
+
+void _main() {
+  cin >> N >> M;
+
+  // N*N, sqrtM
+  vector<vector<vector<pair<int, int>>>> G(N,
+                                           vector<vector<pair<int, int>>>(N));
+
+  set<pair<int, int>> distXY;
+  rep(x, -M, M + 1) {
+    int yy = M - x * x;
+    double y = sqrt(yy);
+    if (y != floor(y)) continue;
+
+    int dx = x, dy = (int)y;
+    if (yy < 0) dy = -dy;
+    rep(i, 0, 2) {
+      if (i == 1) dx = -dx, dy = -dy;
+      distXY.insert({dx, dy});
+    }
+  }
+
+  rep(x, 0, N) {
+    rep(y, 0, N) {
+      fore(d, distXY) {
+        int nx = x + d.first;
+        int ny = y + d.second;
+        if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+          G[x][y].push_back({nx, ny});
+        }
+      }
+    }
+  }
+
+  vector<vector<int>> dist(N, vector<int>(N, -1));
+  dist[0][0] = 0;
+  queue<pair<int, int>> que;
+  que.push({0, 0});
+
+  while (!que.empty()) {
+    pair<int, int> cur = que.front();
+    que.pop();
+
+    for (auto next : G[cur.first][cur.second]) {
+      if (dist[next.first][next.second] == -1) {
+        dist[next.first][next.second] = dist[cur.first][cur.second] + 1;
+        que.push({next.first, next.second});
+      }
+    }
+  }
+
+  for (const auto& el : dist) {
+    for (const auto& e : el) {
+      cout << e << " ";
+    }
+    cout << endl;
+  }
+}
