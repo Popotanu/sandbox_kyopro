e25154dadc1ae751d19135836ecb7e999137fd80
popotanu <scrapmacaron@gmail.com>
2022-10-18 14:21:09 +0900

    [解説AC]abc273_d_LRUD_instructions: グリッドの外枠を壁と同一視する.番兵

--- abc273/d/main.cpp
+++ abc273/d/main.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 /* -------------ACL--------------- */
 // #include <atcoder/fenwicktree>
 // #include <atcoder/segtree>
@@ -42,113 +41,71 @@ template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } 
 
 int H, W, N, ch, cw, Q;
 void _main() {
-  /* 1.準備とか*/
   cin >> H >> W >> ch >> cw >> N;
-
-  // 壁全体の情報を2種類の持ち方で持つ．map.
-  // wallsFixedH[h]={w1,w2,w3...}
-  // wallsFixedW[w]={h1,h2,h3...}
-  // 2つのmapは同じ情報が入ってるけど,軸1つ固定して持つことで衝突判定が楽になる.
-  map<int, vector<int>> wallsFixedH, wallsFixedW;
+  map<int, vector<int>> wFixedH, wFixedW;
   rep(i, 0, N) {
     int h, w;
     cin >> h >> w;
-    wallsFixedH[h].push_back(w);
-    wallsFixedW[w].push_back(h);
+    wFixedH[h].push_back(w);
+    wFixedW[w].push_back(h);
+  }
+  fore(w, wFixedH) {
+    // グリッドの外枠を壁と同一視する
+    // これにより壁と外枠の衝突判定を1つの処理でできる
+    w.second.push_back(0);
+    w.second.push_back(W + 1);
+    sort(all(w.second));
+  }
+  fore(w, wFixedW) {
+    w.second.push_back(0);
+    w.second.push_back(H + 1);
+    sort(all(w.second));
   }
 
-  // lower_boundしたい
-  fore(w, wallsFixedH) sort(all(w.second));
-  fore(w, wallsFixedW) sort(all(w.second));
-
-  /* 2.クエリにこたえる */
   cin >> Q;
-  rep(q, 0, Q) {
+  rep(i, 0, Q) {
     char d;
     int l;
     cin >> d >> l;
 
-    switch (d) {
-      {
-        case 'U':
-          // 障害物(壁)判定. 上に移動するからx軸は固定して考えてOK
-          int wallH = 0;  // 0で初期化することで,グリッド外の衝突判定も兼ねる.
-          auto itW = wallsFixedW.find(cw);
-          if (itW == wallsFixedW.end()) {
-            ch = max(ch - l, wallH + 1);
-            break;
-          }
-
-          vector<int>& wHs = itW->second;  // 値のコピー避ける
-
-          // いま↑に行きたい.壁のうち，現在地よりyの小さいものたちを取ってくる
-          auto it = lower_bound(all(wHs), ch);
-          if (it != wHs.begin()) {
-            // e.g)いまy=3にいる,壁たちのy座標={1,2,4,5}とする
-            // {1,2,| 4,5} ←lower_boundで取れるポインタ
-            // ほしいのは2を指すポインタ,の値. だから1つ戻す.
-            it--;
-
-            // it--したおかげでend()指してた場合でもバグらなくて嬉しい
-            wallH = *it;
-          }
-          ch = max(ch - l, wallH + 1);  // 壁の手前orグリッドの端まで動ける
-          break;
+    if (d == 'U') {
+      int border = 1;
+      auto itW = wFixedW.find(cw);
+      if (itW != wFixedW.end()) {
+        auto itH = lower_bound(all(itW->second), ch);
+        itH--, border = (*itH) + 1;
       }
+      ch = max(ch - l, border);
 
-      {
-        case 'D':
-          int wallH = H + 1;
-          auto itW = wallsFixedW.find(cw);
-          if (itW == wallsFixedW.end()) {
-            ch = min(ch + l, wallH - 1);
-            break;
-          }
-
-          vector<int>& wHs = itW->second;
-          auto itH = upper_bound(all(wHs), ch);
-          if (itH != wHs.end()) wallH = *itH;
-
-          ch = min(ch + l, wallH - 1);
-          break;
+    } else if (d == 'D') {
+      int border = H;
+      auto itW = wFixedW.find(cw);
+      if (itW != wFixedW.end()) {
+        auto itH = lower_bound(all(itW->second), ch);
+        border = (*itH) - 1;
       }
+      ch = min(ch + l, border);
 
-      {
-        case 'L':
-          int wallW = 0;
-          auto itH = wallsFixedH.find(ch);
-          if (itH == wallsFixedH.end()) {
-            cw = max(cw - l, wallW + 1);
-            break;
-          }
-
-          vector<int>& wWs = itH->second;
-          auto itW = lower_bound(all(wWs), cw);
-          if (itW != wWs.begin()) itW--, wallW = *itW;
-          cw = max(cw - l, wallW + 1);
-          break;
+    } else if (d == 'L') {
+      int border = 1;
+      auto itH = wFixedH.find(ch);
+      if (itH != wFixedH.end()) {
+        auto itW = lower_bound(all(itH->second), cw);
+        itW--, border = (*itW) + 1;
       }
+      cw = max(cw - l, border);
 
-      {
-        case 'R': {
-          int wallW = W + 1;
-          auto itH = wallsFixedH.find(ch);
-
-          if (itH == wallsFixedH.end()) {
-            cw = min(cw + l, wallW - 1);
-            break;
-          }
-
-          vector<int>& wWs = itH->second;
-          auto itW = upper_bound(all(wWs), cw);
-          if (itW != wWs.end()) wallW = *itW;
-          cw = min(cw + l, wallW - 1);
-          break;
-        }
-        default:
-          assert(1 == 2);
-          break;
+    } else if (d == 'R') {
+      int border = W;
+      auto itH = wFixedH.find(ch);
+      if (itH != wFixedH.end()) {
+        auto itW = lower_bound(all(itH->second), cw);
+        border = (*itW) - 1;
       }
+      cw = min(cw + l, border);
+
+    } else {
+      assert(1 == 2);
     }
 
     cout << ch << " " << cw << endl;
