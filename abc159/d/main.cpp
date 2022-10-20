24d52313450c697a41304dd9dcd60900e816e26b
popotanu <scrapmacaron@gmail.com>
2022-10-20 22:47:24 +0900

    [AC]abc159_d_bannedK: 組み合わせの総数を持っておいて,各kでAkの組み合わせを引いてく

---
+++ abc159/d/main.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+/* ------------------------------- */
+
+// clang-format off
+
+#ifdef LOCAL
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+  #define dbg(x) true
+#endif
+
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int N, M, Q;
+string S;
+void _main() {
+  cin >> N;
+
+  vector<int> cnt(N + 1);
+  vector<int> A(N);
+  rep(i, 0, N) {
+    int a;
+    cin >> a;
+    cnt[a]++;
+    A[i] = a;
+  }
+
+  vector<ll> per_combi(N + 1, 0);
+
+  ll total = 0;
+  rep(i, 0, cnt.size()) {
+    ll co = 1LL * cnt[i] * (cnt[i] - 1) / 2;
+    total += co;
+    per_combi[i] = co;
+  }
+
+  // ll tes = accumulate(
+  //     per_combi.begin(), per_combi.end(), 0LL,
+  //     [](ll acc, const auto& current) { return 1LL * acc + current; });
+  // assert(total == tes);
+
+  rep(k, 0, N) {
+    int re = A[k];
+    if (per_combi[re] == 0) {
+      std::cout << total << std::endl;
+      continue;
+    }
+    ll raw = per_combi[re];
+    ll di = 1LL * (cnt[re] - 1) * (cnt[re] - 2) / 2;
+
+    std::cout << total - (raw - di) << std::endl;
+  }
+}
