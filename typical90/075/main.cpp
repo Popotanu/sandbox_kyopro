c4b27e55ca8ef8fd4816fb724aca9ecf62edf2d4
popotanu <scrapmacaron@gmail.com>
2022-10-26 14:39:14 +0900

    [AC]typical90_075_magic_for_balls:ACしたけど遅い.各素因数を頂点とした二分木を考える

---
+++ typical90/075/main.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* -------------ACL--------------- */
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+//
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+//
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+//
+// using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+// using mint = modint;
+/* ------------------------------- */
+
+// clang-format off
+
+#ifdef LOCAL
+  #define _GLIBCXX_DEBUG  // 配列外参照をしたときにエラーをあげる(未定義な動作の代わりに)
+  #define dbg(x) cout << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+  #define dbg(x) true
+#endif
+
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+ll N;
+ll count_exp(ll n) {
+  ll result = 0;
+  ll n_cur = n;
+
+  for (int i = 2; 1LL * i * i <= n; i++) {
+    ll ex = 0;
+    while (n_cur % i == 0) {
+      ex++;
+      n_cur /= i;
+    }
+    result += ex;
+  }
+
+  if (n_cur != 1) result++;
+  return result;
+}
+void _main() {
+  cin >> N;
+
+  // 各素因数を頂点とした2分木を考えて,その高さが答え...なはず
+  // 計算に必要なのは指数の情報だけ.
+  std::cout << ceil(log2(count_exp(N))) << std::endl;
+}
